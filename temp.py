b7d553a4aa4511d3f115f86f4b429c121d1abba4
Carson Schmidt <crschmidt2003@gmail.com>
2019-01-03 15:55:08 -0800

    Added Gender Randomizer

--- temp.py
+++ temp.py
@@ -30,18 +30,25 @@ def clear():
 
 # class storing body aspects
 class char_body:
+    def gender_mixer (self):
+        gender_array = ["Male", "Female"]
+        final_gender = random.choice(list(gender_array))
+
+        return final_gender
+        
     def height_mixer (self):
         randheight = random.randint(58,80)
-        inches = randheight % 12
-        sub = randheight - inches
-        feet = sub / 12
 
-        feet = int(feet)
-        inches = int(inches)
+        return randheight
+        
+    def weight_mixer (self, height):
+        # add if statement to dictate a diff ratio between weight and height for women
+        randweight = random.randint(180.0, 200.0)
+        averageweight = height * 2.3
+        non_averaged_weight = averageweight + randweight
+        finalweight = non_averaged_weight / 2
 
-        finalheight = (str(feet) + "' " + str(inches) + "\" ")
-        return finalheight
-    
+        return finalweight
 
 # class storing character backstory
 class char_backstory:
@@ -129,10 +136,29 @@ def gettraits():
 
 def getbody():
     x = char_body()
-    finalweight = x.height_mixer()
 
-    print(finalweight)
+    # get random height
+    randheight = x.height_mixer()
 
+    # get random weight
+    final_weight = x.weight_mixer(randheight)
+
+    # translate height
+    sub = randheight 
+    inches = randheight % 12
+    sub = randheight - inches
+    feet = sub / 12
+
+    feet = int(feet)
+    inches = int(inches)
+
+    final_height = (str(feet) + "' " + str(inches) + "\"")
+
+    final_gender = x.gender_mixer()
+
+    print(final_height)
+    print(final_weight)
+    print(final_gender)
 # Called Functions
 
 getname()
